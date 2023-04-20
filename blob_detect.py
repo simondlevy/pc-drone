c02f682606589bce0249cb6c5ad00f310c9c1a9f
simondlevy <simon.d.levy@gmail.com>
2023-04-20 19:01:35 -0400

    =Cleanup

--- blob_detect.py
+++ blob_detect.py
@@ -5,81 +5,95 @@ import numpy as np
 import itertools
 import math
 
-# original undistort function.  Took 15ms to run. New version below only takes 3ms.
+
+# original undistort function.  Took 15ms to run. New version below only takes
+# 3ms.
 def undistort_crop2(orig_img):
-      
-    #undistort and crop
-    #cv2.undistort(src, cameraMatrix, distCoeffs[, dst[, newCameraMatrix]]) -> dst
+    # undistort and crop
+    # cv2.undistort(src, cameraMatrix, distCoeffs[, dst[,
+    #               newCameraMatrix]]) -> dst
     dst = cv2.undistort(orig_img, mtx, dist, None, newcameramtx)
-    x,y,w,h = roi
-    crop_frame = dst[y:y+h, x:x+w]    
+    x, y, w, h = roi
+    crop_frame = dst[y:y+h, x:x+w]
     return crop_frame
+
 
 # create maps for undistortion
 def init_undistort():
-    #cv2.initUndistortRectifyMap(cameraMatrix, distCoeffs, R, newCameraMatrix, size, m1type[, map1[, map2]]) -> map1, map2
-    frame_size=(640,480)
-    map1, map2=cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, frame_size, cv2.CV_32FC1)
+    # cv2.initUndistortRectifyMap(cameraMatrix, distCoeffs,
+    #                              R, newCameraMatrix, size,
+    #                             m1type[, map1[, map2]]) -> map1, map2
+    frame_size = (640, 480)
+    map1, map2 = cv2.initUndistortRectifyMap(
+            mtx, dist, None, newcameramtx, frame_size, cv2.CV_32FC1)
     return map1, map2
-   
-# this is a faster undistort_crop that only does remapping. Requires call to init_undistort first to
-# to create the map1 and map2   
+
+
+# this is a faster undistort_crop that only does remapping. Requires call to
+# init_undistort first to to create the map1 and map2
 def undistort_crop(orig_img):
-    #cv2.remap(src, map1, map2, interpolation[, dst[, borderMode[, borderValue]]]) -> dst   
+    # cv2.remap(src, map1, map2,
+    #           interpolation[, dst[, borderMode[, borderValue]]]) -> dst
     dst = cv2.remap(orig_img, map1, map2, cv2.INTER_LINEAR)
-    x,y,w,h = roi
-    crop_frame = dst[y:y+h, x:x+w]    
+    x, y, w, h = roi
+    crop_frame = dst[y:y+h, x:x+w]
     return crop_frame
-    
+
+
 def add_blobs(crop_frame):
-    #frame=cv2.GaussianBlur(crop_frame, (3, 3), 0)
-    frame=crop_frame
+
+    # frame = cv2.GaussianBlur(crop_frame, (3, 3), 0)
+    frame = crop_frame
+
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
     # define range of green color in HSV
-    #lower_green = np.array([60,20,20])
-    #upper_green = np.array([80,255,255])
-    
+    # lower_green = np.array([60,20,20])
+    # upper_green = np.array([80,255,255])
+
     # Actually purple
-    lower_green = np.array([115,50,10])
-    upper_green = np.array([160,255,255])
-    
-    
+    lower_green = np.array([115, 50, 10])
+    upper_green = np.array([160, 255, 255])
+
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_green, upper_green)
-    #mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=1)    
+    # mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=1)
+
     # Bitwise-AND mask and original image
-    #res = cv2.bitwise_and(frame,frame, mask= mask)
+    # res = cv2.bitwise_and(frame,frame, mask= mask)
     detector = cv2.SimpleBlobDetector_create(params)
+
     # Detect blobs.
-    reversemask=255-mask
+    reversemask = 255-mask
     keypoints = detector.detect(reversemask)
     if keypoints:
-        print("found blobs")
+        print('found blobs')
         if len(keypoints) > 4:
             keypoints.sort(key=(lambda s: s.size))
-            keypoints=keypoints[0:3]
-        if len(keypoints)==4:
-            pts= np.array([keypoints[i].pt for i in range(4)])
-            #x,y=zip(*pts)
-            
+            keypoints = keypoints[0:3]
+        if len(keypoints) == 4:
+            pts = np.array([keypoints[i].pt for i in range(4)])
+            # x,y=zip(*pts)
+
             # Calculate distances between all combinations of points
-            dis_vectors = [l - r for l, r in itertools.combinations(pts, 2)]
-            dcalc=[np.linalg.norm(dis_vectors[i]) for i in range(6)]
+            dis_vectors = [lft - rgt
+                           for lft, rgt in itertools.combinations(pts, 2)]
+            dcalc = [np.linalg.norm(dis_vectors[i]) for i in range(6)]
 
             # find the closest point to all of them, that is the middle point
-            mean_a = np.array([dcalc[i] for i in [0,1,2]]).sum()/4.0
-            mean_b = np.array([dcalc[i] for i in [0,3,4]]).sum()/4.0
-            mean_c = np.array([dcalc[i] for i in [1,3,5]]).sum()/4.0
-            mean_d = np.array([dcalc[i] for i in [2,4,5]]).sum()/4.0
+            mean_a = np.array([dcalc[i] for i in [0, 1, 2]]).sum()/4.0
+            mean_b = np.array([dcalc[i] for i in [0, 3, 4]]).sum()/4.0
+            mean_c = np.array([dcalc[i] for i in [1, 3, 5]]).sum()/4.0
+            mean_d = np.array([dcalc[i] for i in [2, 4, 5]]).sum()/4.0
             middlepoint = np.argmin(np.array([mean_a, mean_b, mean_c, mean_d]))
 
             # find two furthest points, those are left and right sidepoints
-            idx = np.argmax(dcalc) 
-            max_dist_val = np.max(dcalc)            
+            idx = np.argmax(dcalc)
+            max_dist_val = np.max(dcalc)
 
-            #print(max_dist_val)
+            # print(max_dist_val)
 
             if idx == 0:
                 sidepts = [0, 1]
@@ -139,7 +153,7 @@ def add_blobs(crop_frame):
                     (255, 255, 255),
                     cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-            textstr = ("%0.4f dist. %i,%i center, %0.2f deg" %
+            textstr = ('%0.4f dist. %i,%i center, %0.2f deg' %
                        (max_dist_val, keypoints[middlepoint].pt[0],
                         keypoints[middlepoint].pt[1], theta*180/math.pi))
 
@@ -158,12 +172,12 @@ def add_blobs(crop_frame):
         # (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         else:
             im_with_keypoints = crop_frame
-            print("%i blobs" % (len(keypoints)))
+            print('%i blobs' % (len(keypoints)))
             max_blob_dist = None
             blob_center = None
             theta = None
     else:
-        print("no blobs")
+        print('no blobs')
         im_with_keypoints = crop_frame
         max_blob_dist = None
         blob_center = None

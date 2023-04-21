26f58c0b200ae3318fd0b17bc01dfe646c7ea0c6
simondlevy <simon.d.levy@gmail.com>
2023-04-21 18:58:21 -0400

    =Cleanup

--- fly_drone.py
+++ fly_drone.py
@@ -194,6 +194,7 @@ def add_blobs(crop_frame, params):
     return (im_with_keypoints,
             max_blob_dist, blob_center, theta)  # , keypoint_in_orders
 
+
 def init_params():
 
     # Setup SimpleBlobDetector parameters.
@@ -220,6 +221,7 @@ def init_params():
     params.minInertiaRatio = 0.01
 
     return params
+
 
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
@@ -314,8 +316,8 @@ def flight_sequence(seqname, xseq_list, yseq_list, zseq_list, tseq_list):
         theta_endpoint = tseq[-1] + np.pi / 2
         if (theta_endpoint > np.pi):
             theta_endpoint -= 2*np.pi
-        elif (e_dt < (-np.pi)):
-            theta_endpoint += 2 * np.pi
+        # elif (e_dt < (-np.pi)):  # XXX e_dt undefined
+        #     theta_endpoint += 2 * np.pi
         xseq = np.concatenate((xseq, np.ones(xpoints) * xseq[-1]))
         yseq = np.concatenate((yseq, np.ones(xpoints) * yseq[-1]))
         zseq = np.concatenate((zseq, np.ones(xpoints) * zseq[-1]))
@@ -328,18 +330,18 @@ def flight_sequence(seqname, xseq_list, yseq_list, zseq_list, tseq_list):
         theta_endpoint = tseq[-1] - np.pi/2
         if (theta_endpoint > np.pi):
             theta_endpoint -= 2*np.pi
-        elif (e_dt < (-np.pi)):
-            theta_endpoint += 2 * np.pi
+        # elif (e_dt < (-np.pi)):  # XXX e_dt undefined
+        #     theta_endpoint += 2 * np.pi
         xseq = np.concatenate((xseq, np.ones(xpoints)*xseq[-1]))
         yseq = np.concatenate((yseq, np.ones(xpoints)*yseq[-1]))
         zseq = np.concatenate((zseq, np.ones(xpoints)*zseq[-1]))
         tseq = np.concatenate(
                 (tseq, np.linspace(tseq[-1], theta_endpoint, xpoints)))
+
     return list(xseq), list(yseq), list(zseq), list(tseq)
 
 
 def main():
-
 
     # load calibration data to undistort images
     calfile = np.load('camera_cal_data_2016_03_25_15_23.npz')
@@ -392,9 +394,9 @@ def main():
     xspeed = 0
     yspeed = 0
     zspeed = 0
-    dz_old = 0
-    dx_old = 0
-    dy_old = 0
+    # dz_old = 0
+    # dx_old = 0
+    # dy_old = 0
 
     e_dz = 0
     e_dx = 0
@@ -409,28 +411,26 @@ def main():
     e_d2y = 0
     e_d2t = 0
 
-
-
     THROTTLE_MID = cp.THROTTLE_MID
     ELEVATOR_MID = cp.ELEVATOR_MID
     AILERON_MID = cp.AILERON_MID
     RUDDER_MID = cp.RUDDER_MID
 
-    speeds = ''
+    # speeds = ''
 
-    xpos_target = 300
+    x_target = 300
     ypos_target = 200
     zpos_target = 65
     theta_target = 0  # 45.0/180.0*np.pi
 
-    xpos_targ_seq = [xpos_target]
+    x_targ_seq = [x_target]
     ypos_targ_seq = [ypos_target]
     zpos_targ_seq = [zpos_target]
     theta_targ_seq = [theta_target]
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-    tic = timeit.default_timer()
-    toc = 0
+    # tic = timeit.default_timer()
+    # toc = 0
     flighttic = timeit.default_timer()
     flighttoc = timeit.default_timer()
     flightnum = 0
@@ -466,26 +466,27 @@ def main():
             rval = False
         ii = 100
         while rval:
-            toc_old = toc
-            toc = timeit.default_timer()
+            # toc_old = toc
+            # toc = timeit.default_timer()
             # prints out time since the last frame was read
-            print('deltaT: %0.4f  fps: %0.1f' % (toc - toc_old, 1/(toc-toc_old)))
+            # print('deltaT: %0.4f  fps: %0.1f' %
+            #       (toc - toc_old, 1/(toc-toc_old)))
 
             frame_undistort = undistort_crop(frame_o, map1, map2, roi)
-            toc2 = timeit.default_timer()
-            print('deltaT_execute_undistort: %0.4f' % (toc2 - toc))
+            # toc2 = timeit.default_timer()
+            # print('deltaT_execute_undistort: %0.4f' % (toc2 - toc))
 
             frame, zpos, xypos, theta = add_blobs(frame_undistort, params)
 
-            toc2 = timeit.default_timer()
+            # toc2 = timeit.default_timer()
 
-            print('deltaT_execute_blob_detect: %0.4f' % (toc2 - toc))
+            # print('deltaT_execute_blob_detect: %0.4f' % (toc2 - toc))
 
             if start_flying:
                 try:
                     if flt_mode != LANDING_FM:
-                        print('Zpos: %i Xpos: %i Ypos: %i' %
-                              (zpos, xypos[0], xypos[1]))
+                        # print('Zpos: %i Xpos: %i Ypos: %i' %
+                        #       (zpos, xypos[0], xypos[1]))
                         e_dz_old = e_dz
                         e_dz = zpos-zpos_target
                         e_iz += e_dz
@@ -494,7 +495,7 @@ def main():
                         throttle = (cp.Kz * (e_dz * cp.Kpz + cp.Kiz * e_iz +
                                     cp.Kdz * e_d2z) + THROTTLE_MID)
                         e_dx_old = e_dx
-                        e_dx = xypos[0]-xpos_target
+                        e_dx = xypos[0]-x_target
                         e_ix += e_dx
                         e_ix = clamp(e_ix, -200000, 200000)
                         e_d2x = e_dx - e_dx_old
@@ -530,24 +531,24 @@ def main():
                         e_it = clamp(e_it, -200000, 200000)
                         e_d2t = e_dt-e_dt_old
                         rudder = cp.Kt * (
-                                e_dt * cp.Kpt + cp.Kit * e_it + cp.Kdt * e_d2t) + (
-                                        RUDDER_MID)
+                                e_dt * cp.Kpt + cp.Kit * e_it + cp.Kdt *
+                                e_d2t) + RUDDER_MID
                         if zpos > 0:
-                            print('highalt')
+                            # print('highalt')
                             aileron = clamp(aileron, 1000, 2000)
                             elevator = clamp(elevator, 1000, 2000)
                         else:
-                            print('lowalt')
+                            # print('lowalt')
                             aileron = clamp(aileron, 1400, 1600)
                             elevator = clamp(elevator, 1400, 1600)
                         no_position_cnt = 0
                     else:  # landing mode
                         throttle = throttle-20
 
-                except Exception as e:
-                    print(e)
+                except Exception:
+                    # print(e)
                     no_position_cnt += 1
-                    print('STOPPED. no position or error. ')
+                    # print('STOPPED. no position or error. ')
                     if no_position_cnt > 15:
                         throttle = 1000
                         start_flying = 0
@@ -556,7 +557,7 @@ def main():
             throttle = clamp(throttle, 1000, 2000)
             rudder = clamp(rudder, 1000, 2000)
             command = '%i,%i,%i,%i' % (throttle, aileron, elevator, rudder)
-            print('[PC]: '+command)
+            # print('[PC]: '+command)
             arduino.write((command+'\n').encode())
 
             # Serial comms - read back from Arduino
@@ -567,34 +568,39 @@ def main():
                 data = arduino.readline()
 
             # Monitor keyboard
-            speeds = 'dz:  %+5.2f dx:  %+5.2f  dy: %+5.2f' % (dz, dx, dy)
-            targets = ('tsz: %+5.2f tsx: %+5.2f tsy: %+5.2f' %
-                       (zspeed, xspeed, yspeed))
-            gains = ('Kpz: %+5.2f Kiz: %+5.2f Kdz: %+5.2f' %
-                     (cp.Kpz, cp.Kiz, cp.Kdz))
-            errors_z = ('e_dz: %+5.2f e_iz: %+5.2f e_d2z: %+5.2f' %
-                        (e_dz, e_iz, e_d2z))
+
+            # speeds = 'dz:  %+5.2f dx:  %+5.2f  dy: %+5.2f' % (dz, dx, dy)
+
+            # targets = ('tsz: %+5.2f tsx: %+5.2f tsy: %+5.2f' %
+            #            (zspeed, xspeed, yspeed))
+
+            # gains = ('Kpz: %+5.2f Kiz: %+5.2f Kdz: %+5.2f' %
+            #          (cp.Kpz, cp.Kiz, cp.Kdz))
+
+            # errors_z = ('e_dz: %+5.2f e_iz: %+5.2f e_d2z: %+5.2f' %
+            #             (e_dz, e_iz, e_d2z))
+
             flighttoc = timeit.default_timer()
             cv2.putText(frame, command, (10, 50), font, .8, (255, 255, 255), 2,
                         cv2.LINE_AA)
 
             cv2.putText(frame,
                         'Flt#: {0} Time:{1:0.3f}'.format(
-                            flightnum, flighttoc - flighttic), (10, 75), font, .8,
-                        (255, 255, 255), 2, cv2.LINE_AA)
+                            flightnum, flighttoc - flighttic), (10, 75), font,
+                        .8, (255, 255, 255), 2, cv2.LINE_AA)
 
-            cv2.rectangle(frame, (int(xpos_target)-5, int(ypos_target)-5),
-                          (int(xpos_target)+5, int(ypos_target)+5), (255, 0, 0),
+            cv2.rectangle(frame, (int(x_target)-5, int(ypos_target)-5),
+                          (int(x_target)+5, int(ypos_target)+5), (255, 0, 0),
                           thickness=1, lineType=8, shift=0)
 
             # dst=cv2.resize(frame, (1280,960), cv2.INTER_NEAREST)
             cv2.imshow('preview', frame)
-            toc2 = timeit.default_timer()
-            print('deltaT_execute_imshow: %0.4f' % (toc2 - toc))
+            # toc2 = timeit.default_timer()
+            # print('deltaT_execute_imshow: %0.4f' % (toc2 - toc))
 
             key = cv2.waitKey(wait_time)
-            toc2 = timeit.default_timer()
-            print('deltaT_execute_waitkey: %0.4f' % (toc2 - toc))
+            # toc2 = timeit.default_timer()
+            # print('deltaT_execute_waitkey: %0.4f' % (toc2 - toc))
             # key = ord('0')
             if start_flying:
                 # start recording to video when flying
@@ -605,17 +611,21 @@ def main():
                 if xypos is None:
                     xypos = np.zeros(2)
                     zpos = 0
-                flightdata = np.vstack((flightdata, np.array([flighttoc-flighttic,
-                                        xypos[0], xypos[1], zpos, dx, dy, dz,
-                                        e_dx, e_ix, e_d2x, e_dy, e_iy, e_d2y,
-                                        e_dz, e_iz, e_d2z, xspeed, yspeed, zspeed,
-                                        throttle, aileron, elevator, rudder])))
-                if len(xpos_targ_seq) > 1:
-                    xpos_target = xpos_targ_seq.pop(0)
+                flightdata = np.vstack((flightdata,
+                                        np.array([flighttoc - flighttic,
+                                                  xypos[0], xypos[1],
+                                                  zpos, dx, dy, dz,
+                                                  e_dx, e_ix, e_d2x, e_dy,
+                                                  e_iy, e_d2y, e_dz, e_iz,
+                                                  e_d2z, xspeed, yspeed,
+                                                  zspeed, throttle, aileron,
+                                                  elevator, rudder])))
+                if len(x_targ_seq) > 1:
+                    x_target = x_targ_seq.pop(0)
                     ypos_target = ypos_targ_seq.pop(0)
                     zpos_target = zpos_targ_seq.pop(0)
                     theta_target = theta_targ_seq.pop(0)
-                    print('seq len %i' % len(xpos_targ_seq))
+                    print('seq len %i' % len(x_targ_seq))
                 elif flt_mode == PROGRAM_SEQ_FM:
                     flt_mode = LANDING_FM
 
@@ -624,8 +634,9 @@ def main():
                         '_' + 'flightdata.npy', flightdata)
                 np.save('flight_data\\' + timestamp + '_flt' + str(flightnum) +
                         '_' + 'controldata.npy', controldata)
-                with open('flight_data\\' + timestamp + '_flt' + str(flightnum) +
-                          '_' + 'controlvarnames.npy', 'wb') as f:
+                with open('flight_data\\' + timestamp + '_flt' +
+                          str(flightnum) + '_' + 'controlvarnames.npy',
+                          'wb') as f:
                     pickle.dump(controlvarnames, f)
                 recording_data = 0
 
@@ -679,21 +690,21 @@ def main():
                                    dir(cp) if not item.startswith('__')]
                 controldata = [eval('cp.'+item) for item in controlvarnames]
 
-                xpos_targ_seq = [xpos_target]
+                x_targ_seq = [x_target]
                 ypos_targ_seq = [ypos_target]
                 zpos_targ_seq = [zpos_target]
                 theta_targ_seq = [theta_target]
 
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                    flight_sequence('hover', xpos_targ_seq, ypos_targ_seq,
+                x_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
+                    flight_sequence('hover', x_targ_seq, ypos_targ_seq,
                                     zpos_targ_seq, theta_targ_seq)
 
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                    flight_sequence('right_spot', xpos_targ_seq, ypos_targ_seq,
+                x_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
+                    flight_sequence('right_spot', x_targ_seq, ypos_targ_seq,
                                     zpos_targ_seq, theta_targ_seq)
 
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                    flight_sequence('left_spot', xpos_targ_seq, ypos_targ_seq,
+                x_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
+                    flight_sequence('left_spot', x_targ_seq, ypos_targ_seq,
                                     zpos_targ_seq, theta_targ_seq)
 
                 flt_mode = PROGRAM_SEQ_FM
@@ -710,56 +721,27 @@ def main():
                 arduino.close()
                 arduino = openArduino()
 
-            elif key == ord('1'):
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                        flight_sequence('takeoff', xpos_targ_seq,
-                                        ypos_targ_seq, zpos_targ_seq,
-                                        theta_targ_seq)
+            elif key >= ord('1') and key <= ord('7'):
 
-            elif key == ord('2'):
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                        flight_sequence('land', xpos_targ_seq,
-                                        ypos_targ_seq, zpos_targ_seq,
-                                        theta_targ_seq)
+                commands = ('takeoff', 'land', 'box', 'left_spot',
+                            'right_spot', 'rotate90_left', 'rotate90_right')
 
-            elif key == ord('3'):
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                        flight_sequence('box', xpos_targ_seq, ypos_targ_seq,
-                                        zpos_targ_seq, theta_targ_seq)
+                command = commands[key - ord('1')]
 
-            elif key == ord('4'):
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                        flight_sequence('left_spot', xpos_targ_seq,
+                (x_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq) = (
+                        flight_sequence(command, x_targ_seq,
                                         ypos_targ_seq, zpos_targ_seq,
-                                        theta_targ_seq)
-
-            elif key == ord('5'):
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                        flight_sequence('right_spot', xpos_targ_seq,
-                                        ypos_targ_seq, zpos_targ_seq,
-                                        theta_targ_seq)
-
-            elif key == ord('6'):
-                xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq = \
-                        flight_sequence('rot90_left', xpos_targ_seq,
-                                        ypos_targ_seq, zpos_targ_seq,
-                                        theta_targ_seq)
-
-            elif key == ord('7'):
-                (xpos_targ_seq, ypos_targ_seq, zpos_targ_seq, theta_targ_seq) = \
-                        flight_sequence('rot90_right', xpos_targ_seq,
-                                        ypos_targ_seq, zpos_targ_seq,
-                                        theta_targ_seq)
+                                        theta_targ_seq))
 
             # print out the time needed to execute everything except the image
             # reload
-            toc2 = timeit.default_timer()
-            print('deltaT_execute_other: %0.4f' % (toc2 - toc))
+            # toc2 = timeit.default_timer()
+            # print('deltaT_execute_other: %0.4f' % (toc2 - toc))
 
             # read next frame
             rval, frame_o = vc.read()
-            toc2 = timeit.default_timer()
-            print('deltaT_execute_nextframe: %0.4f' % (toc2 - toc))
+            # toc2 = timeit.default_timer()
+            # print('deltaT_execute_nextframe: %0.4f' % (toc2 - toc))
 
     finally:
         # close the connection

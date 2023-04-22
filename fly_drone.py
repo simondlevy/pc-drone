e9ba712501a49b0685b462a2d0f85a9418406284
simondlevy <simon.d.levy@gmail.com>
2023-04-21 20:04:34 -0400

    =Cleanup

--- fly_drone.py
+++ fly_drone.py
@@ -57,6 +57,98 @@ def undistort_crop(orig_img, map1, map2, roi):
     crop_frame = dst[y:y+h, x:x+w]
     return crop_frame
 
+def handle_good_keypoints(frame, keypoints):
+
+    pts = np.array([keypoints[i].pt for i in range(4)])
+    # x,y=zip(*pts)
+
+    # Calculate distances between all combinations of points
+    dis_vectors = [lft - rgt
+                   for lft, rgt in itertools.combinations(pts, 2)]
+    dcalc = [np.linalg.norm(dis_vectors[i]) for i in range(6)]
+
+    # find the closest point to all of them, that is the middle point
+    mean_a = np.array([dcalc[i] for i in [0, 1, 2]]).sum()/4.0
+    mean_b = np.array([dcalc[i] for i in [0, 3, 4]]).sum()/4.0
+    mean_c = np.array([dcalc[i] for i in [1, 3, 5]]).sum()/4.0
+    mean_d = np.array([dcalc[i] for i in [2, 4, 5]]).sum()/4.0
+    middlepoint = np.argmin(np.array([mean_a, mean_b, mean_c, mean_d]))
+
+    # find two furthest points, those are left and right sidepoints
+    idx = np.argmax(dcalc)
+    max_dist_val = np.max(dcalc)
+
+    # print(max_dist_val)
+
+    if idx == 0:
+        sidepts = [0, 1]
+    elif idx == 1:
+        sidepts = [0, 2]
+    elif idx == 2:
+        sidepts = [0, 3]
+    elif idx == 3:
+        sidepts = [1, 2]
+    elif idx == 4:
+        sidepts = [1, 3]
+    elif idx == 5:
+        sidepts = [2, 3]
+
+    # the frontpoint is the remaining one.
+    frontpoint = 6 - np.array(sidepts+[middlepoint]).sum()
+
+    # now determine which side point is the left one
+    # http://stackoverflow.com/questions/1560492/
+    # how-to-tell-whether-a-point-is-to-the-right-or-left-side-of-a-line
+    a = keypoints[middlepoint].pt
+    b = keypoints[frontpoint].pt
+    c = keypoints[sidepts[0]].pt
+    if ((b[0] - a[0])*(c[1] - a[1]) - (b[1] - a[1])*(c[0] - a[0])) < 0:
+        leftpt = sidepts[0]
+        rightpt = sidepts[1]
+    else:
+        leftpt = sidepts[1]
+        rightpt = sidepts[0]
+
+    # Calculate angle frontpoint - midpoint
+    offset_line = np.array(
+            keypoints[rightpt].pt)-np.array(keypoints[leftpt].pt)
+
+    theta = -np.arctan2(offset_line[1], offset_line[0])
+
+    im_with_midpoint = cv2.drawKeypoints(
+            frame, [keypoints[middlepoint]], np.array([]), (0, 0, 255),
+            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    im_with_midpoint_frontpoint = cv2.drawKeypoints(
+            im_with_midpoint, [keypoints[frontpoint]], np.array([]),
+            (255, 0, 0),
+            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    keypoints_side = [keypoints[i] for i in [leftpt]]
+
+    im_with_keypoints1 = cv2.drawKeypoints(
+            im_with_midpoint_frontpoint, keypoints_side, np.array([]),
+            (0, 255, 0),
+            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    keypoints_side = [keypoints[i] for i in [rightpt]]
+
+    im_with_keypoints = cv2.drawKeypoints(
+            im_with_keypoints1, keypoints_side, np.array([]),
+            (255, 255, 255),
+            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    textstr = ('%0.4f dist. %i,%i center, %0.2f deg' %
+               (max_dist_val, keypoints[middlepoint].pt[0],
+                keypoints[middlepoint].pt[1], theta*180/np.pi))
+
+    max_blob_dist = max_dist_val
+    blob_center = keypoints[middlepoint].pt
+    keypoints[middlepoint].pt[1]
+    # putText(im_with_keypoints, textstr, (10, 25))
+
+    return im_with_keypoints, blob_center, max_blob_dist, theta
+
 
 def add_blobs(crop_frame, params):
 
@@ -86,98 +178,15 @@ def add_blobs(crop_frame, params):
     # Detect blobs.
     reversemask = 255-mask
     keypoints = detector.detect(reversemask)
-    if keypoints:
+
+    if keypoints is not None:
+
         if len(keypoints) > 4:
             keypoints = sorted(keypoints, key=(lambda s: s.size))
             keypoints = keypoints[0:3]
+
         if len(keypoints) == 4:
-            pts = np.array([keypoints[i].pt for i in range(4)])
-            # x,y=zip(*pts)
-
-            # Calculate distances between all combinations of points
-            dis_vectors = [lft - rgt
-                           for lft, rgt in itertools.combinations(pts, 2)]
-            dcalc = [np.linalg.norm(dis_vectors[i]) for i in range(6)]
-
-            # find the closest point to all of them, that is the middle point
-            mean_a = np.array([dcalc[i] for i in [0, 1, 2]]).sum()/4.0
-            mean_b = np.array([dcalc[i] for i in [0, 3, 4]]).sum()/4.0
-            mean_c = np.array([dcalc[i] for i in [1, 3, 5]]).sum()/4.0
-            mean_d = np.array([dcalc[i] for i in [2, 4, 5]]).sum()/4.0
-            middlepoint = np.argmin(np.array([mean_a, mean_b, mean_c, mean_d]))
-
-            # find two furthest points, those are left and right sidepoints
-            idx = np.argmax(dcalc)
-            max_dist_val = np.max(dcalc)
-
-            # print(max_dist_val)
-
-            if idx == 0:
-                sidepts = [0, 1]
-            elif idx == 1:
-                sidepts = [0, 2]
-            elif idx == 2:
-                sidepts = [0, 3]
-            elif idx == 3:
-                sidepts = [1, 2]
-            elif idx == 4:
-                sidepts = [1, 3]
-            elif idx == 5:
-                sidepts = [2, 3]
-
-            # the frontpoint is the remaining one.
-            frontpoint = 6 - np.array(sidepts+[middlepoint]).sum()
-
-            # now determine which side point is the left one
-            # http://stackoverflow.com/questions/1560492/
-            # how-to-tell-whether-a-point-is-to-the-right-or-left-side-of-a-line
-            a = keypoints[middlepoint].pt
-            b = keypoints[frontpoint].pt
-            c = keypoints[sidepts[0]].pt
-            if ((b[0] - a[0])*(c[1] - a[1]) - (b[1] - a[1])*(c[0] - a[0])) < 0:
-                leftpt = sidepts[0]
-                rightpt = sidepts[1]
-            else:
-                leftpt = sidepts[1]
-                rightpt = sidepts[0]
-
-            # Calculate angle frontpoint - midpoint
-            offset_line = np.array(
-                    keypoints[rightpt].pt)-np.array(keypoints[leftpt].pt)
-
-            theta = -np.arctan2(offset_line[1], offset_line[0])
-
-            im_with_midpoint = cv2.drawKeypoints(
-                    frame, [keypoints[middlepoint]], np.array([]), (0, 0, 255),
-                    cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-            im_with_midpoint_frontpoint = cv2.drawKeypoints(
-                    im_with_midpoint, [keypoints[frontpoint]], np.array([]),
-                    (255, 0, 0),
-                    cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-            keypoints_side = [keypoints[i] for i in [leftpt]]
-
-            im_with_keypoints1 = cv2.drawKeypoints(
-                    im_with_midpoint_frontpoint, keypoints_side, np.array([]),
-                    (0, 255, 0),
-                    cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-            keypoints_side = [keypoints[i] for i in [rightpt]]
-
-            im_with_keypoints = cv2.drawKeypoints(
-                    im_with_keypoints1, keypoints_side, np.array([]),
-                    (255, 255, 255),
-                    cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-            textstr = ('%0.4f dist. %i,%i center, %0.2f deg' %
-                       (max_dist_val, keypoints[middlepoint].pt[0],
-                        keypoints[middlepoint].pt[1], theta*180/np.pi))
-
-            max_blob_dist = max_dist_val
-            blob_center = keypoints[middlepoint].pt
-            keypoints[middlepoint].pt[1]
-            # putText(im_with_keypoints, textstr, (10, 25))
+            im_with_keypoints, blob_center, max_blob_dist, theta = handle_good_keypoints(frame, keypoints)
 
         # Draw detected blobs as red circles.
         # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the

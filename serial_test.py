5cb68c9bd9f036c844fed84280e2900a12c26fad
simondlevy <simon.d.levy@gmail.com>
2023-04-25 19:35:49 -0400

    More serial_test.py

--- serial_test.py
+++ serial_test.py
@@ -15,96 +15,115 @@ MIT License
 '''
 import serial, time, kbhit
 
-def step(arduino, kb):
+class SerialTester:
 
-    data = arduino.readline()
+    def __init__(self):
 
-    if data:
+        self.throttle = 1000
+        self.aileron = 1500
+        self.elevator = 1500
+        self.rudder = 1500 # yaw, rotates the drone
 
-        #String responses from Arduino Uno are prefaced with [AU]
-        print('[AU]: '+data)
-        
-    if kb.kbhit():
+        self.tg = 10
+        self.ag = 50
+        self.eg = 50
+        self.rg = 50
 
-        key = ord(kb.getch())
+        self.kb = kbhit.KBHit()
 
-        if key == 27: #ESC
-            print('[PC]: ESC exiting')
-            return
-
-        elif key == 13: #Enter
-            #select()
-            print('[PC]: Enter')
-
-        elif key == 119: #w
-            throttle+=tg
-
-        elif key == 97: #a
-            rudder-=rg         
-
-        elif key == 115: #s
-            throttle-=tg
-
-        elif key == 100: #d
-            rudder+=rg
-
-        elif key == 224: #Special keys (arrows, f keys, ins, del, etc.)
-            key = ord(kb.getch())
-            if key == 80: #Down arrow
-                elevator-=eg
-            elif key == 72: #Up arrow
-                elevator+=eg
-            elif key == 77: #right arroww
-                aileron+=ag
-            elif key == 75: #left arrow
-                aileron-=ag               
-        
-        command='%i,%i,%i,%i'% (throttle, aileron, elevator, rudder)
-        # string commands to the Arduino are prefaced with  [PC]           
-        print('[PC]: '+command)
-        arduino.write(command+'\n')
-
-def main():
-
-    throttle=1000
-    aileron=1500
-    elevator=1500
-    rudder=1500 # yaw, rotates the drone
-
-    tg=10
-    ag=50
-    eg=50
-    rg=50
-
-    kb = kbhit.KBHit()
+        self.arduino = None
 
 
-    try:
+    def begin(self):
 
-        arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=.01)
+        self.arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=.01)
 
         time.sleep(1) #give the connection a second to settle
 
-        #arduino.write('1500, 1500, 1500, 1500\n')
+
+    def step(self):
+
+        data = self.arduino.readline()
+
+        if data:
+
+            #String responses from Arduino Uno are prefaced with [AU]
+            print('[AU]: '+data)
+            
+        if self.kb.kbhit():
+
+            key = ord(self.kb.getch())
+
+            if key == 27: #ESC
+                print('[PC]: ESC exiting')
+                return
+
+            elif key == 13: #Enter
+                #select()
+                print('[PC]: Enter')
+
+            elif key == 119: #w
+                self.throttle+=self.tg
+
+            elif key == 97: #a
+                self.rudder-=self.rg         
+
+            elif key == 115: #s
+                self.throttle-=self.tg
+
+            elif key == 100: #d
+                self.rudder+=self.rg
+
+            elif key == 224: #Special keys (arrows, f keys, ins, del, etc.)
+                key = ord(self.kb.getch())
+                if key == 80: #Down arrow
+                    self.elevator-=eg
+                elif key == 72: #Up arrow
+                    self.elevator+=eg
+                elif key == 77: #right arroww
+                    self.aileron+=ag
+                elif key == 75: #left arrow
+                    self.aileron-=ag               
+            
+            command='%i,%i,%i,%i'% (self.throttle, self.aileron, self.elevator, self.rudder)
+            # string commands to the Arduino are prefaced with  [PC]           
+            print('[PC]: '+command)
+            self.arduino.write(command+'\n')
+
+    def close(self):
+
+        # close the connection
+        self.arduino.close()
+
+        # re-open the serial port which will also reset the Arduino Uno and
+        # this forces the quadcopter to power off when the radio loses conection. 
+        self.arduino=serial.Serial('/dev/ttyACM0', 115200, timeout=.01)
+        self.arduino.close()
+        # close it again so it can be reopened the next time it is run.  
+
+        self.kb.set_normal_term()
+
+def main():
+
+
+    tester = SerialTester()
+
+    try:
+
+        tester.begin()
 
         while True:
 
             try:
-                step(arduino, kb)
+                tester.step()
 
             except KeyboardInterrupt:
+
                 break
             
     finally:
 
-        # close the connection
-        arduino.close()
-        # re-open the serial port which will also reset the Arduino Uno and
-        # this forces the quadcopter to power off when the radio loses conection. 
-        arduino=serial.Serial('/dev/ttyACM0', 115200, timeout=.01)
-        arduino.close()
-        # close it again so it can be reopened the next time it is run.  
+        tester.close()
 
-        kb.set_normal_term()
 
 main()
